0a6effe25c803e75ba223be8104285b2f4fd2960
Kristian Polso <kristian.polso@gmail.com>
2018-03-28 17:26:59 +0300

    Dist & express

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+var port = process.env.PORT || 80;
+app.listen(port);
+
+console.log('server started '+ port);
