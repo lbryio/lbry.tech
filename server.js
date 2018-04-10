6b6fe796b5df1685130f7ae6aa4345120df12fb2
Kristian Polso <kristian.polso@gmail.com>
2018-04-10 12:08:46 +0300

    Fixes #15

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ var sslRedirect = require('heroku-ssl-redirect');
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var request = require('request');
+var cors = require('cors');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
+app.use(cors());
+
 // enable ssl redirect
 app.use(sslRedirect(['other','development', 'staging', 'production']));
+
+app.get('/forward', function(req, res) {
+
+  if(typeof req.query.method != "undefined") {
+
+    // We should whitelist the query parameters here
+
+    request({
+      url: "http://daemon.lbry.tech",
+      qs: req.query
+    }, function(error, response, body) {
+      // Should we parse the body before forwarding?
+      res.setHeader('Content-Type', 'application/json');
+      res.send(body);
+    })
+
+  }
+
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port);
