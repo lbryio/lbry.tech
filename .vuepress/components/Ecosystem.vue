1c9ea16f1187a09e621c8372d9c5e14e820d32cd
ポール ウェッブ <netopwibby@thenetwork.email>
2018-05-28 11:40:05 -0500

    V2 of overview

--- .vuepress/components/Ecosystem.vue
+++ .vuepress/components/Ecosystem.vue
@@ -1,52 +1,101 @@
 <template>
 
   <section class="overview__ecosystem">
-    <div class="overview__ecosystem__module lbrycard">
-      <strong class="__title">lbrycard</strong>
-    </div>
+    <div class="overview__ecosystem__module lbrycard" v-on:click="open('lbrycard')">
+      <h2 class="__title">lbrycard</h2>
 
-    <div class="overview__ecosystem__module lbryschema">
-      <strong class="__title">lbryschema</strong>
-    </div>
+      <div class="overview__ecosystem__module__details">
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit semper luctus praesent phasellus egestas lectus, placerat feugiat arcu dis fermentum maecenas in augue mus quisque euismod justo. Posuere senectus fermentum luctus iaculis nascetur congue enim, semper gravida egestas facilisis mus sociosqu dui metus, est augue mollis phasellus quis leo. Montes hendrerit potenti cubilia feugiat dictum vitae tellus, habitant cursus dignissim et leo morbi, aliquet proin bibendum vivamus per mattis.</p>
 
-    <div class="overview__ecosystem__module lbryumx">
-      <strong class="__title">lbryumx</strong>
-    </div>
-
-    <div class="overview__ecosystem__module lbry">
-      <strong class="__title">lbry</strong>
-    </div>
-
-    <ul class="overview__ecosystem__module applications">
-      <div>
-        <strong class="__title">Applications</strong>
-
-        <li class="overview__ecosystem__module__piece applications__desktop">
-          <strong class="__title">Desktop</strong>
-        </li>
-
-        <li class="overview__ecosystem__module__piece applications__mobile">
-          <strong class="__title">Mobile</strong>
-        </li>
-
-        <li class="overview__ecosystem__module__piece applications__speech">
-          <strong class="__title">spee.ch</strong>
-        </li>
+        <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
       </div>
-    </ul>
-
-    <div class="overview__ecosystem__module lighthouse">
-      <strong class="__title">Lighthouse</strong>
     </div>
 
-    <div class="overview__ecosystem__module chainquery">
-      <strong class="__title">chainquery</strong>
+    <div class="overview__ecosystem__module lbry" v-on:click="open('lbry')">
+      <h2 class="__title">lbry</h2>
+
+      <div class="overview__ecosystem__module__details">
+        <ul>
+          <li>
+            <strong class="__title">lbryschema</strong>
+
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              <li>Himenaeos purus facilisi litora suspendisse molestie, cubilia maecenas faucibus vivamus.</li>
+              <li>Nisi sociosqu interdum augue condimentum vivamus, ac leo feugiat.</li>
+              <li>Justo eu neque aenean nec sagittis, cubilia magnis arcu.</li>
+            </ul>
+          </li>
+
+          <li>
+            <strong class="__title">lbryumx</strong>
+
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              <li>Himenaeos purus facilisi litora suspendisse molestie, cubilia maecenas faucibus vivamus.</li>
+              <li>Nisi sociosqu interdum augue condimentum vivamus, ac leo feugiat.</li>
+              <li>Justo eu neque aenean nec sagittis, cubilia magnis arcu.</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="overview__ecosystem__module applications" v-on:click="open('applications')">
+      <h2 class="__title">Applications</h2>
+
+      <div class="overview__ecosystem__module__details">
+        <ul>
+          <li>
+            <strong class="__title">Desktop</strong>
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+          </li>
+
+          <li>
+            <strong class="__title">Mobile</strong>
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+          </li>
+
+          <li>
+            <strong class="__title">spee.ch</strong>
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+          </li>
+
+          <li>
+            <strong class="__title">Lighthouse</strong>
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+          </li>
+
+          <li>
+            <strong class="__title">chainquery</strong>
+            <p>Vel nisi hendrerit id tristique congue tortor nisl luctus, vulputate sem ridiculus inceptos et at torquent feugiat, mus cursus lobortis aenean senectus posuere odio. Imperdiet nascetur dapibus eget convallis ante donec iaculis dictumst mi est, venenatis quisque integer etiam justo id aliquet non diam semper erat, blandit quis lectus ac aptent magna cubilia augue laoreet. Mollis luctus pharetra nisl auctor potenti magna penatibus cras, justo lobortis iaculis porta a vel habitasse vulputate, taciti sociis arcu facilisis duis orci aliquam.</p>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 
 </template>
 
-
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+      open (ecosystemComponentClassName) {
+        document.querySelectorAll(".overview__ecosystem__module").forEach(n => n.classList.remove("active"));
+        document.getElementsByClassName(ecosystemComponentClassName)[0].classList.add("active");
+      }
+    },
+    name: "Something"
+  }
+</script>
 
 <style lang="scss">
   @import "../scss/init/colors";
