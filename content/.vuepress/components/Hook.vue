df7b5608ffd3c53a7810411419030b581267fbeb
Kristian Polso <kristian.polso@gmail.com>
2018-05-30 12:20:33 +0300

    Move pages to content subfolder

--- content/.vuepress/components/Hook.vue
+++ content/.vuepress/components/Hook.vue
@@ -82,7 +82,7 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "../../node_modules/highlight.js/styles/monokai-sublime";
+  @import "../../../node_modules/highlight.js/styles/monokai-sublime";
   @import "../scss/init/colors";
   @import "../scss/init/extends";
   @import "../scss/init/mixins";
---
+++ content/README.md
@@ -0,0 +1,4 @@
+---
+home: true
+title: LBRY.tech
+---
