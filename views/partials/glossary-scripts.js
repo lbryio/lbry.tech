c0e8a6fbfe80a9e0f54331cd63c5e5050873018c
ポール ウェッブ <netopwibby@thenetwork.email>
2018-07-27 10:18:29 -0500

    Closes #37

--- views/partials/glossary-scripts.js
+++ views/partials/glossary-scripts.js
@@ -1,5 +1,9 @@
-/* global document */ "use strict";
+/* global $, document */ "use strict";
 
 
 
 document.getElementsByTagName("body")[0].classList.add("glossary");
+
+$("[data-action='toggle glossary sidebar']").on("click", () => { // Toggle sidebar
+  $("body").toggleClass("sidebar-closed");
+});
