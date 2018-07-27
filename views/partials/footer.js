7b1aff1e68dce60513a2562f3cc81437a2afd9ed
ポール ウェッブ <netopwibby@thenetwork.email>
2018-07-27 13:33:27 -0500

    Added Google Analytics

--- views/partials/footer.js
+++ views/partials/footer.js
@@ -9,6 +9,7 @@ const local = require("app-root-path").require;
 
 //  V A R I A B L E S
 
+const config = local("/config");
 const editLink = local("/views/partials/edit-link");
 const emailSubscribe = local("/views/partials/email-subscribe");
 
@@ -51,4 +52,14 @@ module.exports = exports = state => html`
   </footer>
 
   <script src="/assets/scripts/app.js"></script>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+
+     ga("create", ${config.ga}, "auto");
+     ga("send", "pageview");
+  </script>
 `;
