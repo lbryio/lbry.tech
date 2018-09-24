3ba7557d0170fad9d7c49593511d3238bf8052c7
Jeremy Kauffman <kauffj@gmail.com>
2018-09-24 17:14:50 -0400

    fix and refactor names

--- app/components/link-grid.js
+++ app/components/link-grid.js
@@ -4,10 +4,10 @@ import html from "choo/html";
 
 function returnLinkTemplate(title, description, destination, label) {
   return `
-    <li class="home__feature">
-     <p class="home__feature__title"><strong>${title}</strong></p>
-     <p class="home__feature__description">${description}</p>
-     <a class="home__feature__cta" href="${destination}">${label}</a>
+    <li class="link-grid__link">
+     <p class="link-grid__title"><strong>${title}</strong></p>
+     <p class="link-grid__description">${description}</p>
+     <a class="link-grid__cta" href="${destination}">${label}</a>
     </li>
   `;
 }
@@ -15,8 +15,8 @@ function returnLinkTemplate(title, description, destination, label) {
 export default (links) => {
   const renderedLinks = links.map((link) => returnLinkTemplate(link.title, link.description, link.destination, link.label));
   return html`
-    <ul class="home__features">
-      ${renderedLinks.join("\n")}
+    <ul class="link-grid">
+      ${renderedLinks}
     </ul>
   `;
 }
