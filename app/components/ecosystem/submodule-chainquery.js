66a675806ef10f11a7cbdc251e7aa75bebf31474
Jeremy Kauffman <kauffj@gmail.com>
2018-09-25 12:34:13 -0400

    decouple markdown and page content, simplify css

--- app/components/ecosystem/submodule-chainquery.js
+++ app/components/ecosystem/submodule-chainquery.js
@@ -9,17 +9,18 @@ module.exports = exports = () => `
     <h3 class="ecosystem__submodule__title" data-action="openSubmodule" data-target="chainquery">chainquery</h3>
 
     <div class="ecosystem__submodule__description">
-      <h4 class="ecosystem__submodule__description__title">Overview</h4>
-      <p>The model of Chainquery at its foundation consists of the fundamental data types found in the blockchain. This information is then expounded on with additional columns and tables that make querying the data much easier.</p>
-
-      <h4 class="ecosystem__submodule__description__title __connection">Connection to...</h4>
-      <p class="__connection-details"></p>
-
-      <h4 class="ecosystem__submodule__description__title">Source</h4>
-      <ul>
-        <li><a href="https://github.com/lbryio/chainquery" title="chainquery source code">https://github.com/lbryio/chainquery</a></li>
-      </ul>
-
+      <div class="ecosystem__submodule__markdown">
+        <h4 class="ecosystem__submodule__description__title">Overview</h4>
+        <p>The model of Chainquery at its foundation consists of the fundamental data types found in the blockchain. This information is then expounded on with additional columns and tables that make querying the data much easier.</p>
+  
+        <h4 class="ecosystem__submodule__description__title __connection">Connection to...</h4>
+        <p class="__connection-details"></p>
+  
+        <h4 class="ecosystem__submodule__description__title">Source</h4>
+        <ul>
+          <li><a href="https://github.com/lbryio/chainquery" title="chainquery source code">https://github.com/lbryio/chainquery</a></li>
+        </ul>
+      </div>
       <ul class="__parents">
         <li class="__parent green" data-action="open" data-target="applications">Applications</li>
         <li class="__parent red" data-action="open" data-target="lbrycrd">Blockchain</li>
--- app/components/ecosystem/submodule-lighthouse.js
+++ app/components/ecosystem/submodule-lighthouse.js
@@ -9,16 +9,18 @@ module.exports = exports = () => `
     <h3 class="ecosystem__submodule__title" data-action="openSubmodule" data-target="lighthouse">lighthouse</h3>
 
     <div class="ecosystem__submodule__description">
-      <h4 class="ecosystem__submodule__description__title">Overview</h4>
-      <p>Lighthouse is a lightning-fast advanced search engine API for publications on the lbrycrd with autocomplete capabilities.</p>
-
-      <h4 class="ecosystem__submodule__description__title __connection">Connection to...</h4>
-      <p class="__connection-details"></p>
-
-      <h4 class="ecosystem__submodule__description__title">Source</h4>
-      <ul>
-        <li><a href="https://github.com/lbryio/lighthouse" title="lighthouse source code">https://github.com/lbryio/lighthouse</a></li>
-      </ul>
+      <div class="ecosystem__submodule__markdown">
+        <h4 class="ecosystem__submodule__description__title">Overview</h4>
+        <p>Lighthouse is a lightning-fast advanced search engine API for publications on the lbrycrd with autocomplete capabilities.</p>
+  
+        <h4 class="ecosystem__submodule__description__title __connection">Connection to...</h4>
+        <p class="__connection-details"></p>
+  
+        <h4 class="ecosystem__submodule__description__title">Source</h4>
+        <ul>
+          <li><a href="https://github.com/lbryio/lighthouse" title="lighthouse source code">https://github.com/lbryio/lighthouse</a></li>
+        </ul>
+      </div>
 
       <ul class="__parents">
         <li class="__parent green" data-action="open" data-target="applications">Applications</li>
