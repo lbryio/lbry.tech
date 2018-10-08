8a20651804d2cb8a22d7e61cf4919b9569c65b2d
Jeremy Kauffman <kauffj@gmail.com>
2018-10-08 10:38:11 -0400

    remove unnecessary class

--- app/components/mission-statement.js
+++ app/components/mission-statement.js
@@ -13,11 +13,11 @@ import html from "choo/html";
 export default () => html`
   <div class="component--mission-statement">
     <strong class="component--mission-statement__title">Mission Statement</strong>
-    To create a market for accessing and publishing information<sup><a class="__plain" href="#footnote-1" title="First footnote of our mission statement">1</a></sup>
-    that is global<sup><a class="__plain" href="#footnote-2" title="Second footnote of our mission statement">2</a></sup>,
-    decentralized<sup><a class="__plain" href="#footnote-3" title="Third footnote of our mission statement">3</a></sup>,
-    robust<sup><a class="__plain" href="#footnote-4" title="Fourth footnote of our mission statement">4</a></sup>,
-    optimal<sup><a class="__plain" href="#footnote-5" title="Fifth footnote of our mission statement">5</a></sup>
-    and complete<sup><a class="__plain" href="#footnote-6" title="Sixth footnote of our mission statement">6</a></sup>.
+    To create a market for accessing and publishing information<sup><a href="#footnote-1" title="First footnote of our mission statement">1</a></sup>
+    that is global<sup><a href="#footnote-2" title="Second footnote of our mission statement">2</a></sup>,
+    decentralized<sup><a href="#footnote-3" title="Third footnote of our mission statement">3</a></sup>,
+    robust<sup><a href="#footnote-4" title="Fourth footnote of our mission statement">4</a></sup>,
+    optimal<sup><a href="#footnote-5" title="Fifth footnote of our mission statement">5</a></sup>
+    and complete<sup><a href="#footnote-6" title="Sixth footnote of our mission statement">6</a></sup>.
   </div>
 `;
