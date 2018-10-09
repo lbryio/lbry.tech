5cc4c9325a5decc445895069e2f04d4c13533ac9
ポール ウェッブ <netopwibby@thenetwork.email>
2018-10-09 16:28:18 -0500

    Closes #169

--- app/components/feature-links.js
+++ app/components/feature-links.js
@@ -7,33 +7,38 @@
 const links = [ // TODO: Update images
   {
     href: "https://chat.lbry.io",
-    image: "http://static.simpledesktops.com/uploads/desktops/2016/12/05/Untitled-1-03-01.png",
+    image: "https://spee.ch/b432346bfd641990f3dc3dbceda057b4ededa360/discord-bg.jpg",
     title: "Chat (Discord)"
   },
   {
     href: "https://discourse.lbry.io",
-    image: "http://static.simpledesktops.com/uploads/desktops/2017/02/28/GeoShapes_2880x1800.png",
+    image: "https://spee.ch/e59dcbe6a533934eb82e73bb335b7e43cb8a9f7d/atmosphere.png",
     title: "Developer Forum"
   },
   {
     href: "https://lbry.fund",
-    image: "http://static.simpledesktops.com/uploads/desktops/2015/08/20/Sunset_by_Banned.png",
+    image: "https://spee.ch/b29b4cb6495c9c2e6fc26113bcc92158ed408a35/pixelsunset.png",
     title: "lbry.fund (Project Funding)"
   },
   {
     href: "https://www.reddit.com/r/lbry",
-    image: "http://static.simpledesktops.com/uploads/desktops/2016/08/28/Wind-Vector-resize.png",
+    image: "https://spee.ch/2357ab1464bcbba3458f4eabdad9644bcfd6f491/lines.png",
     title: "Reddit"
   },
   {
     href: "https://lbry.io/join-us",
-    image: "http://static.simpledesktops.com/uploads/desktops/2015/09/25/Siri.png",
+    image: "https://spee.ch/6db68b3ebf22386fcd9d04237d11bfaff5ba2a95/minimalisticrubikcube.png",
     title: "Join Our Team"
   },
   {
     href: "https://twitter.com/lbryio",
-    image: "http://static.simpledesktops.com/static/images/sd-bg.png",
+    image: "https://spee.ch/9c38db124b85736adbcca48cdf34877d2110bbcd/GeoShapes.png",
     title: "Twitter"
+  },
+  {
+    href: "https://www.facebook.com/lbryio",
+    image: "https://spee.ch/734404b89658e30af920829553b22d1742c0d99a/voice.png",
+    title: "Facebook"
   }
 ];
 
--- app/sockets.js
+++ app/sockets.js
@@ -181,15 +181,15 @@ function generateMemeCreator(socket) {
   const images = [
     {
       alt: "Carl Sagan",
-      src: "/assets/media/images/carlsagan2.jpg"
+      src: "https://spee.ch/4f6b953e605a602434246743fd246d3e1fd4f5fd/carlsagan2.jpg"
     },
     {
       alt: "Doge",
-      src: "/assets/media/images/doge-meme.jpg"
+      src: "https://spee.ch/2f90f2d91441a4d33d3d4eb82bdfc4c56ec742c7/doge-meme.jpg"
     },
     {
       alt: "LBRY Logo With Green Background",
-      src: "/assets/media/images/lbry-green.png"
+      src: "https://spee.ch/40ac6818bbac87a208722bf4467653341d460908/lbry-green.png"
     }
   ];
 
