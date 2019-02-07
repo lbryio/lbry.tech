241926c649435223919e84ab720df4bdd53742fb
ポール ウェッブ <netopwibby@thenetwork.email>
2019-02-07 11:50:04 -0600

    Removed double-scrolling on /spec

--- app/views/spec.js
+++ app/views/spec.js
@@ -12,6 +12,10 @@ import html from "choo/html";
 
 export default state => {
   state.hideFooter = true;
+  state.lbry = {
+    title: "LBRY Specification",
+    description: "A detailed specification of the LBRY protocol. Learn exactly what LBRY is and how it works!"
+  };
 
   return html`
     <div style="width: 100%; height: calc(100vh - 4rem)">
@@ -22,6 +26,8 @@ export default state => {
       const specDomain = "https://spec.lbry.io";
       const spec = document.getElementById("spec");
       spec.src = specDomain + window.location.hash;
+
+      document.querySelector("body").style["overflow-y"] = "hidden";
 
       window.addEventListener("message", event => {
         if (event.origin !== specDomain || event.source !== spec.contentWindow) // security
