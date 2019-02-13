3c4d9e87293f6e05b20f109c0bc860c8fb8cbbcf
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2019-02-13 13:48:25 -0500

    update explorer links
    This will bring them directly to the tx page, with the publish / tip highlighted.

--- app/helpers/fetch-metadata.js
+++ app/helpers/fetch-metadata.js
@@ -276,7 +276,7 @@ function memePublishMessaging(source) {
 
       <br/><br/>
 
-      To see Proof of Work (lol) that your meme is on the LBRY blockchain, <a href="https://explorer.lbry.io/address/${source.result.claim_address}" rel="noopener noreferrer" target="_blank" title="Your meme, on our blockchain explorer">check it out</a> on our blockchain explorer! Please note that it may take a couple minutes for the transaction to be confirmed.
+      To see Proof of Work (lol) that your meme is on the LBRY blockchain, <a href="https://explorer.lbry.io/tx/${source.result.txid}?address=${source.result.claim_address}" rel="noopener noreferrer" target="_blank" title="Your meme, on our blockchain explorer">check it out</a> on our blockchain explorer! Please note that it may take a couple minutes for the transaction to be confirmed.
 
       <br/><br/>
 
@@ -290,7 +290,7 @@ function memePublishMessaging(source) {
 function tipCompletionMessaging(source) {
   return `
     <p class="playground__description success">
-      If you want proof of the tip you just gave on behalf of LBRY, <a href="https://explorer.lbry.io/tx/${source.result.txid}" rel="noopener noreferrer" target="_blank" title="Your tip, on our blockchain explorer">check it out</a> on our blockchain explorer! Please note that it may take a couple minutes for the transaction to be confirmed.
+      If you want proof of the tip you just gave on behalf of LBRY, <a href="https://explorer.lbry.io/tx/${source.result.txid}?address=${source.result.outputs[0].address}" rel="noopener noreferrer" target="_blank" title="Your tip, on our blockchain explorer">check it out</a> on our blockchain explorer! Please note that it may take a couple minutes for the transaction to be confirmed.
     </p><br/>
   `;
 }
